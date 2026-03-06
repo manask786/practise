6c22ee61227de2ae9367e84b73384e81ce3927ba
Manas Saini <146158203+manask786@users.noreply.github.com>
2026-03-06 23:40:27 +0530

    Add files via upload

---
+++ exception_handling/StudentNotFound.java
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+package exception_handling;
+
+public class StudentNotFound extends Exception {
+
+    public StudentNotFound(String message) {
+        super(message);
+    }
+}
+=======
+package exception_handling;
+
+public class StudentNotFound extends Exception {
+
+    public StudentNotFound(String message) {
+        super(message);
+    }
+}
+>>>>>>> 6f7087364b89a709ae8ec3ebe8929a4733372ffc
